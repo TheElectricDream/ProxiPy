bb56be636481ac02f7fe75c3d1db08bc8f8b367c
Alexander Crain <68435427+TheElectricDream@users.noreply.github.com>
2025-03-10 14:18:47 -0400

    Added lib

---
+++ lib/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+
+# This file indicates that the directory is a Python package.
+# You can import modules from this package using the package name.
+
+# Example of importing a module within this package
+# from .module_name import some_function
